6efc711676ef4e8c5ee029aadec0f271c95a62b2
Joe Catera <zhouyaoji@yahoo.com>
2016-03-02 00:09:20 -0800

    Added the client-side library moment.js so time can be displayed in chat.

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,17 @@ socket.on('connect', function() {
   console.log("Connected!");
 });
 socket.on('message', function(message) {
+  var momentTimestamp = moment.utc(message.timestamp); 
   console.log("New message:");
   console.log(message.user);
   console.log(message.text);
-  jQuery('.messages').append("<p>&nbsp;<b>" + message.user + ":</b> " + message.text + "</p>");
+  console.log(message.time);
+  
+  jQuery('.messages').append("<p>[" + momentTimestamp.local().format('h:mm a') + "] &nbsp;<b>" + message.user + ":</b> " + message.text + "</p>");
 });
-socket.on('welcome', function(message) {
-  jQuery(".messages").append("<p><strong>&nbsp;" + message.text + "</strong></p>");
+  socket.on('welcome', function(message) {
+    var momentTimestamp = moment.utc(message.timestamp); 
+    jQuery(".messages").append("<p>[" + momentTimestamp.local().format('h:mm a') + "] <strong>&nbsp;" + message.text + "</strong></p>");
 });
 
 // Handles submitting of new message.
@@ -18,11 +22,13 @@ var $form = jQuery('#message-form');
 
 $form.on('submit', function(ev) {
   ev.preventDefault();     
+  
   var $message = $form.find('input[name="message"]');
   var $user = $form.find('input[name="username"]');
   socket.emit('message', {
     user: $user.val(),
-    text: $message.val()
+    text: $message.val(),
+    timestamp: moment().valueOf()
   });
   $message.val("");
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require("moment");
 
 app.use(express.static(__dirname + "/public"));
 io.on('connection', function (socket) {
@@ -10,10 +11,14 @@ io.on('connection', function (socket) {
    
   socket.on('message', function(message) {
      console.log("Message received from " + message.user + "\nMessage: "  + message.text);
+     message.timestamp = moment().valueOf();
      io.emit('message', message);
   });
+  
+  
   socket.emit('welcome', {
-      text: 'Welcome to the chat application!'
+      text: 'Welcome to the chat application!',
+      timestamp: moment().valueOf()
   });
 });
 
