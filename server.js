78cb598bd4f106e31a2a6ce332ead673423f85cd
Joe Catera <zhouyaoji@yahoo.com>
2016-03-11 22:16:28 -0800

    Added currentUsers command.

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ var moment = require("moment");
 app.use(express.static(__dirname + "/public"));
 
 var clientInfo = {};
+// Sends current user to provided socket
+function sendCurrentUsers (socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+    if (typeof info ==- 'undefined') {
+       return;
+    }
+    Object.keys(clientInfo).forEach(function (socketId) {
+       var userInfo = clientInfo[socketId];
+       if (info.room === userInfo.room) {
+          users.push(userInfo.name);
+       } 
+    });
+    socket.emit('message', {
+       name: "System",
+       text: "Current users: " + users.join(', '),
+       timestamp: moment().valueOf()
+     });
+}
 
 io.on('connection', function (socket) {
   console.log('User connected via socket.io');
@@ -35,8 +54,12 @@ io.on('connection', function (socket) {
   });
   socket.on('message', function(message) {
      console.log("Message received from " + message.user + "\nMessage: "  + message.text);
-     message.timestamp = moment().valueOf();
-     io.to(clientInfo[socket.id].room).emit('message', message);
+     if(message.text === "@currentUsers") {
+           sendCurrentUsers(socket);
+     } else {
+       message.timestamp = moment().valueOf();
+       io.to(clientInfo[socket.id].room).emit('message', message);
+     }
   });
   
   
